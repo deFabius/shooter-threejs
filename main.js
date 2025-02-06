2ecb85a8d277847a85c74d49940af15f5a22fc5e
Fabio Valle <8283960+deFabius@users.noreply.github.com>
2025-02-06 00:36:10 +0000

    add window resize handler

--- main.js
+++ main.js
@@ -202,3 +202,9 @@ const isPowerupEligible = () => {
   }
   return false;
 };
+
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
